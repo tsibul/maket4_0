f5fdc98e36cff98e97e5079ef33078e104f872b3
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-27 23:55:30 +0300

    request on pure sql

---
+++ src/main/java/ru/maket/maket4_0/controller/jsonController/NextRecordsController.java
@@ -0,0 +1,71 @@
+package ru.maket.maket4_0.controller.jsonController;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.SingularAttribute;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.maket.maket4_0.db.dictionary.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.maket.maket4_0.db.dictionary.sqbuilder.NextTwentyRecordsSQLBuilder;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+import static ru.maket.maket4_0.db.dictionary.DictionaryList.dictionaryList;
+
+@Controller
+public class NextRecordsController {
+
+    @Autowired
+    private EntityManager entityManager;
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public NextRecordsController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @GetMapping("/next_20/{className}/{lastRecord}/{searchString}/{shDeleted}")
+    @ResponseBody
+    public ResponseEntity<List<Map<String, Object>>> nextRecordJson(@PathVariable String className, @PathVariable String lastRecord,
+                                                                    @PathVariable String searchString, @PathVariable String shDeleted)
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<? extends MaketDictionary> dictClass = dictionaryList().get(className);
+        EntityType<?> entityType = entityManager.getMetamodel().entity(dictClass);
+        Set<SingularAttribute<?, ?>> fieldSet = (Set<SingularAttribute<?, ?>>) entityType.getSingularAttributes();
+        NextTwentyRecordsSQLBuilder nextTwentyRecordsSQLBuilder =
+                new NextTwentyRecordsSQLBuilder(fieldSet, className, shDeleted, searchString, lastRecord);
+        String sqlRequest = nextTwentyRecordsSQLBuilder.totalSQLRequest();
+        List<Map<String, Object>> itemsList = jdbcTemplate.queryForList(sqlRequest);
+
+        return ResponseEntity.ok(itemsList);
+    }
+
+
+    private static ArrayList<HashMap<String, Object>> reformatQueryToMap(List records, Set<SingularAttribute<?, ?>> fieldSet) throws NoSuchFieldException, IllegalAccessException {
+        ArrayList<HashMap<String, Object>> hashMapList = new ArrayList<>();
+        for (Object record : records) {
+            HashMap<String, Object> map = new HashMap<>();
+            Field fieldR = null;
+            for (SingularAttribute<?, ?> field : fieldSet) {
+                String attributeName = field.getName();
+                try {
+                    fieldR = record.getClass().getDeclaredField(attributeName);
+                } catch (Exception e) {
+                    fieldR = record.getClass().getSuperclass().getDeclaredField(attributeName);
+                }
+                fieldR.setAccessible(true);
+                String value = fieldR.get(record).toString();
+                map.put(attributeName, value);
+            }
+            hashMapList.add(map);
+        }
+        return hashMapList;
+    }
+
+}
---
+++ src/main/java/ru/maket/maket4_0/db/dictionary/sqbuilder/NextTwentyRecordsSQLBuilder.java
@@ -0,0 +1,146 @@
+package ru.maket.maket4_0.db.dictionary.sqbuilder;
+
+import jakarta.persistence.metamodel.SingularAttribute;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.maket.maket4_0.db.dictionary.MaketDictionary;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+import static ru.maket.maket4_0.db.dictionary.DictionaryList.dictionaryList;
+import static ru.maket.maket4_0.db.dictionary.FieldNames.dataBaseNames;
+import static ru.maket.maket4_0.db.dictionary.FieldNames.fieldNames;
+
+public class NextTwentyRecordsSQLBuilder implements SQLRequestBuilder {
+    private final JdbcTemplate jdbcTemplate;
+    private final Class<? extends MaketDictionary> classType;
+    private final Integer lastRecord;
+    private final String searchString;
+    private final boolean deleted;
+    private final Set<SingularAttribute<?, ?>> fieldSet;
+
+
+    public NextTwentyRecordsSQLBuilder(Set<SingularAttribute<?, ?>> fieldSet, String className, String deleted, String searchString, String lastRecord) {
+        this.jdbcTemplate = new JdbcTemplate();
+        this.classType = dictionaryList().get(className);
+        this.lastRecord = Integer.parseInt(lastRecord);
+        this.deleted = !"0".equals(deleted);
+        this.searchString = "'%" + searchString.replace("_", " ") + "%'";
+        this.fieldSet = fieldSet;
+    }
+
+    private HashMap<String, ArrayList<String[]>> fieldList() {
+        HashMap<String, ArrayList<String[]>> fields = new HashMap<>();
+        ArrayList<String[]> basic = new ArrayList<>();
+        ArrayList<String[]> foreign = new ArrayList<>();
+        ArrayList<String[]> other = new ArrayList<>();
+        fields.put("Basic", basic);
+        fields.put("Foreign", foreign);
+        fields.put("Other", other);
+        fieldSet.forEach(field -> {
+            boolean fieldBasic = field.getPersistentAttributeType().toString().equals("BASIC");
+            boolean fieldForeign = field.getPersistentAttributeType().toString().equals("MANY_TO_ONE");
+            String fieldSimpleType = field.getJavaType().getSimpleName();
+            String fieldName = field.getName();
+            if (fieldNames().get(fieldName) != null) {
+                fieldName = fieldNames().get(fieldName);
+            }
+            if (fieldBasic && fieldSimpleType.equals("String") || fieldSimpleType.equals("Date")) {
+                basic.add(new String[]{fieldName});
+            } else if (fieldForeign) {
+                foreign.add(new String[]{fieldName, field.getName()});
+//                EntityType<?> foreignEntityType = (EntityType<?>) field.getType();
+//                Set<SingularAttribute<?, ?>> foreignFieldSet = (Set<SingularAttribute<?, ?>>) foreignEntityType.getSingularAttributes();
+            } else {
+                other.add(new String[]{fieldName});
+            }
+        });
+        return fields;
+    }
+
+    @Override
+    public String selectFromBuilder() {
+        StringBuilder selectString = new StringBuilder();
+        selectString.append(" SELECT ");
+        for (int i = 0; i < fieldList().get("Other").size(); i++) {
+            selectString.append("c.").append(fieldList().get("Other").get(i)[0]).append(", ");
+        }
+        for (int i = 0; i < fieldList().get("Basic").size(); i++) {
+            selectString.append("c.").append(fieldList().get("Basic").get(i)[0]).append(", ");
+        }
+        int foreignSize = fieldList().get("Foreign").size();
+        if (foreignSize > 0) {
+            for (int i = 0; i < foreignSize; i++) {
+                selectString.append("c").append(i).append(".public_name as ").append(fieldList().get("Foreign").get(i)[1])
+                        .append(", ").append("c").append(i).append(".id as ").append(fieldList().get("Foreign").get(i)[1])
+                        .append("_id").append(", ");
+            }
+        }
+        selectString.delete(selectString.length() - 2, selectString.length());
+
+        selectString.append(" FROM ");
+        if (dataBaseNames().get(classType.getSimpleName()) != null) {
+            String fromName = dataBaseNames().get(classType.getSimpleName());
+            selectString.append(fromName).append(" as c ");
+        } else {
+            selectString.append(classType.getSimpleName()).append(" as c ");
+        }
+        return selectString.toString();
+    }
+
+    @Override
+    public String joinBuilder() {
+        StringBuilder joinString = new StringBuilder();
+        int foreignSize = fieldList().get("Foreign").size();
+        if (foreignSize > 0) {
+            joinString.append(" JOIN ");
+            for (Integer i = 0; i < foreignSize; i++) {
+                joinString.append(fieldList().get("Foreign").get(i)[0]).append(" as c").append(i).append(" ")
+                        .append(" ON c.").append(fieldList().get("Foreign").get(i)[0]).append(" = ").append("c")
+                        .append(i).append(".id, ");
+            }
+            joinString.delete(joinString.length() - 2, joinString.length());
+        }
+        return joinString.toString();
+    }
+
+    @Override
+    public String whereBuilder() {
+        StringBuilder whereString = new StringBuilder();
+        if (!deleted) {
+            whereString.append(" WHERE c.deleted = false ");
+        }
+        if (!searchString.equals("'%default%'")) {
+            whereString.append("AND (");
+            for (int i = 0; i < fieldList().get("Basic").size(); i++) {
+                whereString.append("c.").append(fieldList().get("Basic").get(i)[0])
+                        .append(" LIKE ").append(searchString).append(" OR ");
+            }
+            int foreignSize = fieldList().get("Foreign").size();
+            if (foreignSize > 0) {
+                for (int i = 0; i < foreignSize; i++) {
+                    whereString.append("c").append(i).append(".public_name LIKE ").append(searchString).append(" OR ");
+                }
+            }
+            whereString.delete(whereString.length() - 4, whereString.length());
+            whereString.append(") ");
+        }
+        return whereString.toString();
+    }
+
+    @Override
+    public String orderByBuilder() throws
+            NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        String orderString = (String) classType.getMethod("defaultOrderString").invoke(null);
+        return " ORDER BY " + orderString + " ";
+    }
+
+    @Override
+    public String limitOffsetBuilder() {
+        return " LIMIT 20 OFFSET " + lastRecord + " ";
+    }
+
+
+}
