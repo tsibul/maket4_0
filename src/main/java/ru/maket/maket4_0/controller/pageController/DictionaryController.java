e8afee67045600ec62a4d6760006f0f42553a90e
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-27 23:40:52 +0300

    initial commit (moved)

--- src/main/java/ru/maket/maket4_0/controller/pageController/DictionaryController.java
+++ src/main/java/ru/maket/maket4_0/controller/pageController/DictionaryController.java
@@ -1,4 +1,4 @@
-package ru.maket.maket4_0.controller;
+package ru.maket.maket4_0.controller.pageController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
