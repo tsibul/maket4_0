ddb1c7b6ca578d5c5669e4da0d4977c0abe76b79
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-27 23:53:47 +0300

    changed url

--- src/main/resources/static/js/func/saveDictionaryRecord.js
+++ src/main/resources/static/js/func/saveDictionaryRecord.js
@@ -11,6 +11,6 @@ export function saveDictionaryRecord(obj) {
     event.preventDefault();
     const updateForm = obj.closest('.form-row');
     const dictionaryType = updateForm.parentElement.id.split('-')[0];
-    const fetchPath = '/production/dict_update/' + dictList[dictionaryType];
+    const fetchPath = 'dict_update/' + dictList[dictionaryType];
     saveEditForm(updateForm, fetchPath)
 }
