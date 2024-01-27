fb5ab79c6fa29995dc7b3b4e61bae3343823011d
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-27 23:59:25 +0300

    cleanUp

--- src/main/java/ru/maket/maket4_0/db/dictionary/color/repository/ColorRepository.java
+++ src/main/java/ru/maket/maket4_0/db/dictionary/color/repository/ColorRepository.java
@@ -1,12 +1,9 @@
 package ru.maket.maket4_0.db.dictionary.color.repository;
 
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import ru.maket.maket4_0.db.dictionary.MaketDictionaryRepository;
 import ru.maket.maket4_0.db.dictionary.color.Color;
 
-import java.util.List;
 
-public interface ColorRepository extends JpaRepository<Color, Long>, MaketDictionaryRepository<Color> {
-    List<Color> findByDeletedFalse(Sort sort);
+public interface ColorRepository extends  MaketDictionaryRepository<Color>{
 }
--- src/main/java/ru/maket/maket4_0/db/dictionary/color/repository/ColorSchemeRepository.java
+++ src/main/java/ru/maket/maket4_0/db/dictionary/color/repository/ColorSchemeRepository.java
@@ -1,13 +1,10 @@
 package ru.maket.maket4_0.db.dictionary.color.repository;
 
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import ru.maket.maket4_0.db.dictionary.MaketDictionaryRepository;
 import ru.maket.maket4_0.db.dictionary.color.ColorScheme;
 
-import java.util.List;
+public interface ColorSchemeRepository extends MaketDictionaryRepository<ColorScheme> {
 
-public interface ColorSchemeRepository extends JpaRepository<ColorScheme, Long>, MaketDictionaryRepository<ColorScheme> {
-    List<ColorScheme> findByDeletedFalse(Sort sort);
 }
 
