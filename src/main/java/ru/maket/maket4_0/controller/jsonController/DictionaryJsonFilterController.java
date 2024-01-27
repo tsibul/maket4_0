508c04700157bd72dfe8bf6f196fb91f7b9de4f0
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-27 23:57:01 +0300

    Controller for Criteria list of id+publicName (used in dropdown)

---
+++ src/main/java/ru/maket/maket4_0/controller/jsonController/DictionaryJsonFilterController.java
@@ -0,0 +1,51 @@
+package ru.maket.maket4_0.controller.jsonController;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.SingularAttribute;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.maket.maket4_0.db.dictionary.MaketDictionary;
+import ru.maket.maket4_0.db.dictionary.sqbuilder.CriteriaDictionaryItemListBuilder;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+import static ru.maket.maket4_0.db.dictionary.DictionaryList.dictionaryList;
+
+@Controller
+public class DictionaryJsonFilterController {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @GetMapping("/dictionary_json_filter/{className}")
+    @ResponseBody
+    public ResponseEntity<List<LinkedHashMap<String, Object>>> dictionaryItemsList(@PathVariable String className)
+            throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        Class<? extends MaketDictionary> dictClass = dictionaryList().get(className);
+        EntityType<?> entityType = entityManager.getMetamodel().entity(dictClass);
+        Set<SingularAttribute<?, ?>> fieldSet = (Set<SingularAttribute<?, ?>>) entityType.getSingularAttributes();
+        CriteriaDictionaryItemListBuilder criteriaDictionaryItemListBuilder =
+                new CriteriaDictionaryItemListBuilder(fieldSet, className, entityManager);
+        TypedQuery<MaketDictionary> criteriaQuery = criteriaDictionaryItemListBuilder.buildCriteriaQuery();
+        List<LinkedHashMap<String, Object>> itemsList = criteriaQuery.getResultStream()
+                .map(item -> {
+                    LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+                    map.put("id", item.getId());
+                    map.put("publicName", item.getPublicName());
+                    return map;
+                })
+                .toList();
+
+        return ResponseEntity.ok(itemsList);
+
+
+    }
+
+}
