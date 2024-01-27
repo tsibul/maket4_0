2568f7425b9a7cf946ef104f52c5870f58dc20e0
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-27 23:38:00 +0300

    initial commit (moved)

--- src/main/java/ru/maket/maket4_0/controller/pageController/FilmController.java
+++ src/main/java/ru/maket/maket4_0/controller/pageController/FilmController.java
@@ -1,19 +1,19 @@
-package ru.maket.maket4_0.controller;
+package ru.maket.maket4_0.controller.pageController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TemplateController {
+public class FilmController {
 
-    public TemplateController(){
+    public FilmController(){
     }
 
-    @GetMapping("/template")
-    public String templatePage(Model model) {
+    @GetMapping("/film")
+    public String filmPage(Model model) {
         model.addAttribute("activePage", "film");
-        return "template";
+        return "film";
     }
 
 }
--- src/main/java/ru/maket/maket4_0/controller/pageController/TemplateController.java
+++ src/main/java/ru/maket/maket4_0/controller/pageController/TemplateController.java
@@ -1,19 +1,19 @@
-package ru.maket.maket4_0.controller;
+package ru.maket.maket4_0.controller.pageController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class FilmController {
+public class TemplateController {
 
-    public FilmController(){
+    public TemplateController(){
     }
 
-    @GetMapping("/film")
-    public String filmPage(Model model) {
+    @GetMapping("/template")
+    public String templatePage(Model model) {
         model.addAttribute("activePage", "film");
-        return "film";
+        return "template";
     }
 
 }
