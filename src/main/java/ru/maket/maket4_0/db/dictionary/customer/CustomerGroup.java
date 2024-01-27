cf154f2413bbc960750a7c973f64070bd114014e
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-27 23:35:55 +0300

    added Entity

--- src/main/java/ru/maket/maket4_0/db/dictionary/customer/CustomerGroup.java
+++ src/main/java/ru/maket/maket4_0/db/dictionary/customer/CustomerGroup.java
@@ -1,6 +1,8 @@
 package ru.maket.maket4_0.db.dictionary.customer;
 
+import jakarta.persistence.Entity;
 import ru.maket.maket4_0.db.dictionary.MaketDictionary;
 
+@Entity
 public class CustomerGroup extends MaketDictionary {
 }
