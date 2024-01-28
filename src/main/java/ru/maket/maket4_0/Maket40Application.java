eb464ae0cd172a612a0bb43d4fa446f874f58f73
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-29 00:01:29 +0300

    add global dataSource

--- src/main/java/ru/maket/maket4_0/Maket40Application.java
+++ src/main/java/ru/maket/maket4_0/Maket40Application.java
@@ -2,13 +2,21 @@ package ru.maket.maket4_0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Maket40Application {
 
-	public static void main(String[] args) {
+	public static DataSource dataSource;
 
-		SpringApplication.run(Maket40Application.class, args);
+
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Maket40Application.class, args);
+		dataSource = context.getBean(DataSource.class);
+
 	}
 
 }
