c10b8e0cb8ed5e9355a7099efd321a949535a12a
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-29 00:03:14 +0300

    remove unused import

--- src/main/java/ru/maket/maket4_0/db/dictionary/MaketDictionaryRepository.java
+++ src/main/java/ru/maket/maket4_0/db/dictionary/MaketDictionaryRepository.java
@@ -1,14 +1,11 @@
 package ru.maket.maket4_0.db.dictionary;
 
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 
 public interface MaketDictionaryRepository<T extends MaketDictionary> extends CrudRepository<MaketDictionary, Long>
 {
 //    List<T> findByDeletedFalse(Sort sort);
-//    T findById(Long id);
+//    Optional<MaketDictionary> findById(Long id);
 
 }
